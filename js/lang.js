931e8e2d4b49bd90d05dd42e631a1c032e3a8436
Ivan <138053343+kooolami@users.noreply.github.com>
2023-11-02 00:59:37 +0300

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+let lang = navigator.languages[0] || 'en-EN'
+if(lang == 'ru-RU') {
+	document.getElementById("quantumDescription").innerHTML = ' — профессиональный Discord-бот, способный предоставить весь необходимый функционал серверу в его развитии: <span style="color: rgb(255, 255, 255, 0.7)">экономика</span>, <span style="color: rgb(255, 255, 255, 0.7)">AI-модерация</span>, <span style="color: rgb(255, 255, 255, 0.7)">интерактивные игры</span>, <span style="color: rgb(255, 255, 255, 0.7)">приветствия</span>, <span style="color: rgb(255, 255, 255, 0.7)">программирование<br>слеш-команд</span> и многое другое. Попробуйте!'
+	document.getElementById("inviteButton").innerHTML = 'Пригласить'
+	document.getElementById("aboutButton").innerHTML = 'Подробнее'
+
+	document.getElementById("funcDesignName").innerHTML = ' ･ Дизайн бота'
+	document.getElementById("funcDesignDescription").innerHTML = 'Начните знакомство с ботом со свежим и уникальным дизайном. Разработчики вдохновлялись Леонардо да Винчи.'
+
+	document.getElementById("funcModerationName").innerHTML = ' ･ AI-модерация'
+	document.getElementById("funcModerationDescription").innerHTML = 'Защитите свой Discord-сервер при помощи AI-модерации бота с двумя степенями проверки. Долой нарушителей!'
+
+	document.getElementById("funcSlashName").innerHTML = ' ･ Кастомные слеш-команды'
+	document.getElementById("funcSlashDescription").innerHTML = 'Ощутите себя программистом, создавая свои собственные слеш-команды, которые уже поддерживают немало возможностей.'
+
+	document.getElementById("funcGamesName").innerHTML = ' ･ Интерактивные игры'
+	document.getElementById("funcGamesDescription").innerHTML = 'Максимально повысьте активность на Вашем сервере при помощи тех самых игр, с которыми можно будет делать невообразимые вещи..'
+
+	document.getElementById("funcSearchName").innerHTML = ' ･ Поиск нужной функции'
+	document.getElementById("funcSearchDescription").innerHTML = 'Найдите нужную функцию, используя поиск. Понимает даже мелкие ошибки в тексте или же неправильную раскладку.'
+
+	document.getElementById("funcPermsName").innerHTML = ' ･ Настройка прав команды'
+	document.getElementById("funcPermsDescription").innerHTML = 'Теперь Вы можете устанавливать, кто сможет пользоваться той или иной командой. Это будет особенно удобно серверам, которые как раз таки используют иную систему модерации.'
+
+	document.getElementById("funcMoreName").innerHTML = 'Удивительно! Но это ещё не всё...'
+	document.getElementById("funcMoreDescription").innerHTML = 'Описана только 1/3 часть функционала бота. А чтобы узнать о нём больше - пригласите его на свой Discord-сервер и ощутите всю мощь бота.'
+	document.getElementById("buttonAddToDiscord").innerHTML = 'Добавить в Discord'
+
+	document.getElementById("shortDescriptionQuantum").innerHTML = 'Профессиональный помощник для Вашего сервера, имеющий весь необходимый функционал для его развития.'
+	document.getElementById("footerMonitorings").innerHTML = 'Мониторинги'
+	document.getElementById("footerLinks").innerHTML = 'Полезные Ссылки'
+	document.getElementById("footerAnother").innerHTML = 'Остальное'
+
+	document.getElementById("footerServer").innerHTML = 'Сервер поддержки'
+	document.getElementById("footerTerms").innerHTML = 'Политика использования' 
+	document.getElementById("footerPrivacy").innerHTML = 'Конфиденциальность'
+
+	document.getElementById("footerSecret").innerHTML = '© 2023 Все права защищены'
+}
